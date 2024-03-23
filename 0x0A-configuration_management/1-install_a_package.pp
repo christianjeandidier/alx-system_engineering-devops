c708f0f5925480f51d3e7683324ee1afdad9db8d
christianjeandidier <ndo.jeandidier@gmail.com>
2024-03-22 18:42:12 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install an especific version of werkzeug (2.1.1)
+# Install an especific version of werkzeug (2.1.0)
 package {'werkzeug':
-  ensure   => '2.1.0',
+  ensure   => '2.1.1',
   provider => 'pip3'
 }

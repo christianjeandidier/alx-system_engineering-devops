9c18d4fbaf829d7a5f967036be7929b8781f9369
christianjeandidier <ndo.jeandidier@gmail.com>
2024-03-22 20:16:41 -0700

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,13 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-# Install an especific version of Werkzeug (2.1.1)
-package { 'Werkzeug':
-  ensure => '2.1.1',
-  provider => 'pip3',
-}
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+# install flask using Puppet
 
+exec { 'install_flask':
+    command => '/usr/bin/pip3 install flask==2.1.0',
+    path    => ['/usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+}
+
+exec { 'install_werkzeug':
+    command => '/usr/bin/pip3 install werkzeug==2.1.1',
+    path    => ['/usr/bin', '/bin'],
+    unless  => '/usr/bin/pip3 show werkzeug | grep -q "Version: 2.1.1"',
 }

0fa70c4c078c90dac172ecc228cbf2014a971779
christianjeandidier <ndo.jeandidier@gmail.com>
2024-03-22 19:35:36 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Installs flask from pip3
-package { 'Flask':
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }

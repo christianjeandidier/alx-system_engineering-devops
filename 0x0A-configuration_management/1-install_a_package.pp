71c5995a1ca9225a55c291c6403337bdb5b85db9
christianjeandidier <ndo.jeandidier@gmail.com>
2024-03-22 18:48:14 -0700

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
-# Install an especific version of werkzeug (2.1.1)
-package {'werkzeug':
+# Install an especific version of flask (2.1.1)
+package {'flask':
   ensure   => '2.1.1',
   provider => 'pip3'
 }
